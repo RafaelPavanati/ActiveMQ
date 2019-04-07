7ee310ce4cf054450a275146d2cc58cfb47bfc6c
RafaelPavanati <rpypk.kc86x>
2019-04-06 23:17:19 -0300

    config

--- src/main/java/com/cicloi/activemq/connection/Receiver.java
+++ src/main/java/com/cicloi/activemq/connection/Receiver.java
@@ -1,11 +1,13 @@
 package com.cicloi.activemq.connection;
 
-import java.util.concurrent.CountDownLatch;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
+
+import javax.jms.MapMessage;
+import javax.jms.Message;
+
 
 @Component
 public class Receiver {
@@ -13,19 +15,13 @@ public class Receiver {
     private static final Logger LOGGER =
             LoggerFactory.getLogger(Receiver.class);
 
-    private CountDownLatch latch = new CountDownLatch(1);
-
-    public CountDownLatch getLatch() {
-        return latch;
-    }
-
     /**
-     * Definimos aqui um conteinerFactory, pois nossa aplicação pode ter mais de uma configuração
+     * Podemos  aqui definir um conteinerFactory, pois nossa aplicação pode ter mais de uma configuração
      * e se conectar a mais de um broker
      */
-    @JmsListener(destination = "helloworld.q", containerFactory = "localConection")
-    public void receive(String message) {
+
+    @JmsListener(destination = "helloworld.q")
+    public void onMessage(Message message) {
         LOGGER.info("received message: '{}'", message);
-        latch.countDown();
     }
 }
--- src/main/java/com/cicloi/activemq/connection/Sender.java
+++ src/main/java/com/cicloi/activemq/connection/Sender.java
@@ -4,25 +4,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Component;
 
-import javax.jms.Message;
+import javax.jms.*;
 import java.util.UUID;
 
-@Component
-public class Sender {
+public abstract class Sender {
+
     private static final Logger LOGGER =
             LoggerFactory.getLogger(Sender.class);
+
+    /**
+     * Podemos definir a fila que a mensagem vai ser enviada
+     */
+    abstract String getQueue();
 
     @Autowired
     private JmsTemplate jmsTemplate;
 
-    public void send(String message) {
-        LOGGER.info("sending message='{}'", message);
-        jmsTemplate.send(session -> {
-            Message msg = session.createMessage();
-            msg.setJMSMessageID(UUID.randomUUID().toString());
+    public void send(Object object) {
 
+        jmsTemplate.setDefaultDestinationName(getQueue());
+
+        jmsTemplate.send(session -> {
+
+            MapMessage msg = session.createMapMessage();
+
+            msg.setJMSMessageID(UUID.randomUUID().toString());
             /**
              * Ao definir JMSXGroupID As mensagens em um grupo de mensagens são sempre consumidas pelo mesmo consumidor, mesmo se
              * houver muitos consumidores em uma fila. Eles fixam todas as mensagens com o mesmo ID de grupo para o mesmo consumidor.
